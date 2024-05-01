cea8af56177a9e02b1bf8b0b06c6b8c1d62b1705
Leonel Balduino <leonel.balduinO@gmail.com>
2024-05-01 14:17:51 -0300

    feat: adicionando o restante dos buscadores

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -9,20 +9,29 @@ use Chuva\Php\WebScrapping\Entity\Person;
  * Does the scrapping of a webpage.
  */
 class Scrapper {
+
   private $queryPaperId = '//div[@class="volume-info"]';
+  private $queryPaperTitle = './/h4[@class="my-xs paper-title"]';
+  private $queryPaperType = '//div[@class="tags mr-sm"]';
+  private $queryPerson = '//div[@class="authors"]';
+
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
     
     $xPath = new \DOMXPath($dom);
-    $domIdList = $xPath->query($this->queryPaperId);
+
+    $domPaperIdList = $xPath->query($this->queryPaperId);
+    $domPaperTitleList = $xPath->query($this->queryPaperTitle);
+    $domPaperTypeList = $xPath->query($this->queryPaperType);
+    $domPersonList = $xPath->query($this->queryPerson);
 
     return [
       new Paper(
-        $domIdList[1]->textContent,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
+        $domPaperIdList[0]->textContent,
+        $domPaperTitleList[0]->textContent,
+        $domPaperTypeList[0]->textContent,
         [
           new Person('Katalin Karikó', 'Szeged University'),
           new Person('Drew Weissman', 'University of Pennsylvania'),
