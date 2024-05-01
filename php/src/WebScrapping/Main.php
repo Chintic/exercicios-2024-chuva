2c5650c144b00d1b3818ee1d4747322855a171f3
Leonel Balduino <leonel.balduinO@gmail.com>
2024-05-01 16:09:35 -0300

    feat: adicionando lógica para o Writer do Spout

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,6 +2,10 @@
 
 namespace Chuva\Php\WebScrapping;
 
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+use Box\Spout\Common\Entity\Row;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+
 /**
  * Runner for the Webscrapping exercice.
  */
@@ -16,9 +20,63 @@ class Main {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
-    $data = (new Scrapper())->scrap($dom);
+    $papers = (new Scrapper())->scrap($dom);
 
     // Write your logic to save the output file bellow.
+    
+    $writer = WriterEntityFactory::createXLSXWriter();  
+      
+    $filePath = __DIR__ . '/../../assets/model.xlsx';
+    $writer->openToFile($filePath);
+
+    $headerStyle = (new StyleBuilder())
+      ->setFontBold()
+      ->setFontName('Arial')      
+      ->setFontSize(10)
+      ->build();
+
+    $contentStyle = (new StyleBuilder())
+      ->setFontName('Arial')
+      ->setFontSize(10)
+      ->build();
+    
+    $header = [
+      WriterEntityFactory::createCell('ID'),
+      WriterEntityFactory::createCell('Title'),
+      WriterEntityFactory::createCell('Type')
+    ];
+    
+    for ($i = 1; $i <= $biggestAuthorNumber; $i++) {
+      $header[] = WriterEntityFactory::createCell("Author $i");
+      $header[] = WriterEntityFactory::createCell("Author $i Institution");
+    }
+    
+    $headerRow = WriterEntityFactory::createRow($header, $headerStyle);
+    $writer->addRow($headerRow);
+
+    $paperRows = [];
+    for ($i = 0; $i < count($papers); $i++) {
+      $paper = $papers[$i];
+      $rowData = [
+        $paper->getId(),
+        $paper->getTitle(),
+        $paper->getType()
+      ];
+
+      $authors = $paper->getAuthors();
+      for ($j = 0; $j < count($authors); $j++) {
+        $author = $authors[$j];
+        $rowData[] = $author->getName();
+        $rowData[] = $author->getInstitution();
+      }
+
+      $paperRows[] = WriterEntityFactory::createRowFromArray($rowData, $contentStyle);
+    }
+
+    $writer->addRows($paperRows);
+
+
+    $writer->close();
     print_r($data);
   }
 
