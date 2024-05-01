bb6c13e8ffc395c6670daf42995e5a325d18be0e
Leonel Balduino <leonel.balduinO@gmail.com>
2024-05-01 14:07:43 -0300

    feat: adicionando buscador de ID para pessoa

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -9,14 +9,18 @@ use Chuva\Php\WebScrapping\Entity\Person;
  * Does the scrapping of a webpage.
  */
 class Scrapper {
-
+  private $queryPaperId = '//div[@class="volume-info"]';
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
+    
+    $xPath = new \DOMXPath($dom);
+    $domIdList = $xPath->query($this->queryPaperId);
+
     return [
       new Paper(
-        123,
+        $domIdList[1]->textContent,
         'The Nobel Prize in Physiology or Medicine 2023',
         'Nobel Prize',
         [
